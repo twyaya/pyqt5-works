6fd4bb74b3ffa76347d4d08601d03affd960732a
twyaya <51527338+twyaya@users.noreply.github.com>
2019-10-28 20:33:05 +0800

    Add files via upload

---
+++ Body Mass Index (BMI) Calculator/Window.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Main_Window.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(827, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(280, 210, 421, 181))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.layoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.layoutWidget)
+        self.pushButton.setMinimumSize(QtCore.QSize(50, 50))
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.horizontalLayout.addItem(spacerItem)
+        self.label_2 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.BMI = QtWidgets.QLabel(self.layoutWidget)
+        self.BMI.setEnabled(True)
+        self.BMI.setText("")
+        self.BMI.setObjectName("BMI")
+        self.horizontalLayout.addWidget(self.BMI)
+        self.label_5 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_5.setObjectName("label_5")
+        self.horizontalLayout.addWidget(self.label_5)
+        self.label_4 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        self.horizontalLayout.addWidget(self.label_4)
+        self.layoutWidget1 = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget1.setGeometry(QtCore.QRect(50, 207, 211, 181))
+        self.layoutWidget1.setObjectName("layoutWidget1")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.layoutWidget1)
+        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.label = QtWidgets.QLabel(self.layoutWidget1)
+        self.label.setText("體重(公斤)")
+        self.label.setObjectName("label")
+        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.layoutWidget1)
+        self.label_3.setText("身高(公分)")
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_3.addWidget(self.label_3, 1, 0, 1, 1)
+        self.weight = QtWidgets.QLineEdit(self.layoutWidget1)
+        self.weight.setObjectName("weight")
+        self.gridLayout_3.addWidget(self.weight, 0, 1, 1, 1)
+        self.height = QtWidgets.QLineEdit(self.layoutWidget1)
+        self.height.setObjectName("height")
+        self.gridLayout_3.addWidget(self.height, 1, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 827, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "BMI計算器"))
+        self.pushButton.setText(_translate("MainWindow", "完成"))
+        self.label_2.setText(_translate("MainWindow", "您的BMI為:"))
+        self.label_5.setText(_translate("MainWindow", "分析:"))
+
