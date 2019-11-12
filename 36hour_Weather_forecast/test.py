59a8d849464f087cf0f17050ab9198a5963ec23e
張皓偉 <otis8496@gmail.com>
2019-11-12 21:57:20 +0800

    新增測試
    新增測試

---
+++ 36hour_Weather_forecast/test.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 21:04:17 2019
+
+@author: user
+"""
+
+import pyautogui, time
+
+
+time.sleep(2)
+
+pyautogui.moveTo(876 ,628)
+pyautogui.click()
+time.sleep(1)
+
+try:
+    for i in range(22):
+        pyautogui.moveTo(886, 385)  #選像欄
+        pyautogui.click()
+        pyautogui.keyDown('down')
+        time.sleep(1)
+
+
+        pyautogui.moveTo(876 ,628)
+        pyautogui.click()
+        time.sleep(1)
+
+        pyautogui.moveTo(1004, 624)
+        pyautogui.click()
+        
+        
+        
+except KeyboardInterrupt:               # 處理 Ctrl-C 按鍵
+  print('\nDone.')
+  
